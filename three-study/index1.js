d468f5485dc4793069f8381e4027bfc306ac5d62
hzc <3111267307>
2023-10-10 18:13:58 +0800

    submit

--- three-study/index1.js
+++ three-study/index1.js
@@ -1,26 +1,48 @@
-import * as THREE from 'three'
+// 引入three.js
+import * as THREE from 'three';
+/**
+ * 创建3D场景对象Scene
+ */
+const scene = new THREE.Scene();
 
-const scene = new THREE.Scene()
+/**
+ * 创建网格模型
+ */
+//创建一个长方体几何对象Geometry
+const geometry = new THREE.BoxGeometry(100, 100, 100);
 
-const geometry = new THREE.BoxGeometry(50, 50, 50)
-
+//材质对象Material
 const material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00, //设置材质颜色
-    transparent: true, //开启透明
-    opacity: 0.5 //设置透明度
+    // color: 0x0000ff, //设置材质颜色
+    color: 0x00ffff, //设置材质颜色
+    // transparent: true, //开启透明
+    // opacity: 0.5 //设置透明度
 })
 
-const mesh = new THREE.Mesh(geometry, material)
+//网格模型对象Mesh
+const mesh = new THREE.Mesh(geometry, material);
 
-mesh.position.set(0, 0, 0)
+//设置网格模型在三维空间中的位置坐标，默认是坐标原点
+mesh.position.set(0,10,0);
 
-scene.add(mesh)
-
-console.log('三维场景', scene)
+//网格模型添加到场景中
+scene.add(mesh);
 
 // AxesHelper：辅助观察的坐标系
 const axesHelper = new THREE.AxesHelper(100)
 scene.add(axesHelper)
+
+/**
+ * 光源设置
+ */
+// 点光源
+// const pointLight = new THREE.PointLight(0xffffff, 1.0)
+// const pointLight = new THREE.PointLight(0xffffff, 1, 0, 0);
+const pointLight = new THREE.PointLight(0x0000ff, 1, 0, 0);
+//点光源位置
+// pointLight.position.set(400, 0, 0)
+pointLight.position.set(400, 200, 300)
+scene.add(pointLight)
 
 /**
  * 透视投影相机
@@ -35,7 +57,8 @@ const camera = new THREE.PerspectiveCamera(30, width / height, 1, 3000);
 
 //相机在Three.js三维坐标系中的位置
 // 根据需要设置相机位置具体值
-camera.position.set(200, 200, 200);
+// camera.position.set(200, 200, 200);
+camera.position.set(292, 223, 185);
 
 //相机观察目标指向Threejs 3D空间中某个位置
 camera.lookAt(0, 0, 0); //坐标原点
